ed81f67fab1d2fb00eae99a5d7a0108806f83d86
Manuel Markwort <mma@msdmarkwort.de>
2018-05-25 13:42:03 +0200

    Added support for possible values in StringEditor

--- Marvin.Runtime.Maintenance.Web.UI/src/modules/components/ConfigEditor/StringEditor.tsx
+++ Marvin.Runtime.Maintenance.Web.UI/src/modules/components/ConfigEditor/StringEditor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Input } from "reactstrap";
 import { toString } from "../../models/EntryValueType";
+import EnumEditor from "./EnumEditor";
 import InputEditorBase, { IInputEditorBasePropModel } from "./InputEditorBase";
 
 export default class StringEditor extends InputEditorBase {
@@ -9,11 +10,20 @@ export default class StringEditor extends InputEditorBase {
         this.state = { };
     }
 
-    public render() {
+    private preRenderInput() {
         return (<Input type="text"
                         onChange={(e: React.FormEvent<HTMLInputElement>) => this.onValueChange(e, this.props.Entry)}
                         placeholder={"Please enter a string ..."}
                         disabled={this.props.Entry.Value.IsReadOnly}
                         value={this.props.Entry.Value.Current == null ? "" : this.props.Entry.Value.Current} />);
+    }
+
+    private preRenderPossibleValueList() {
+        return (<EnumEditor Entry={this.props.Entry} />);
+    }
+
+    public render() {
+        return this.props.Entry.Value.Possible != null && this.props.Entry.Value.Possible.length > 0 ?
+                this.preRenderPossibleValueList() : this.preRenderInput();
     }
 }
