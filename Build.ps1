c627b52b249608650fe0a0bb6214bc481a2fa8a1
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2020-07-01 10:31:01 +0200

    Github and CI integration

---
+++ Build.ps1
@@ -0,0 +1,37 @@
+﻿param (
+    [switch]$SetAssemblyVersion,
+    [switch]$Build,
+
+    [switch]$GenerateDocs,
+
+    [switch]$Pack,
+    [switch]$Publish
+)
+
+# Load Toolkit
+. ".build\BuildToolkit.ps1"
+
+# Initialize Toolkit
+Invoke-Initialize -Version (Get-Content "VERSION");
+
+if ($SetAssemblyVersion) {
+    Set-AssemblyVersions;
+}
+
+if ($Build) {
+    Invoke-Build ".\Maintenance.Web.sln"
+}
+
+if ($GenerateDocs) {
+    Invoke-DocFx
+}
+
+if ($Pack) {
+    Invoke-PackAll -Symbols
+}
+
+if ($Publish) {
+    Invoke-Publish
+}
+
+Write-Host "Success!"
